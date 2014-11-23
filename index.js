11f9d0099ca8a4537cc2fa674f445e92fcaaf138
Luke Horvat <lukehorvat@gmail.com>
2014-11-23 23:42:31 +1100

    Implemented function to convert MusicXML to MP3

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 "use strict";
+
+var fs = require("fs");
+var lame = require("lame");
+var xml2pcm = require("musicxml-to-pcm");
+
+module.exports.convert = function(srcFile, destFile) {
+  var bitsPerSample = 16; // A 16-bit integer will represent each sample.
+  var samplesPerSecond = 44100;
+
+  return xml2pcm
+  .newStream(fs.readFileSync(srcFile, "utf8"), bitsPerSample, samplesPerSecond)
+  .pipe(new lame.Encoder({ channels: 1, bitDepth: bitsPerSample, sampleRate: samplesPerSecond }))
+  .pipe(fs.createWriteStream(destFile));
+};
